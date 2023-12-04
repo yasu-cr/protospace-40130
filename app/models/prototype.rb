d27afc1915fa975eaf91a7fbd25285e2b1592b02
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-04 18:03:23 +0900

    削除した場合のエラー解消

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,8 @@
 class Prototype < ApplicationRecord
   belongs_to :user
 
-  has_one_attached :image, dependent: :destroy
-  has_many :comments
+  has_one_attached :image
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :catch_copy, presence: true
