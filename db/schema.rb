a24df1506fd9c244f0f2606335bcc1ccea9d5646
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-04 12:43:48 +0900

    不要なマイグレーションファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -64,13 +64,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_02_142918) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "profile"
-    t.text "position"
-    t.string "name"
-    t.text "occupation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
